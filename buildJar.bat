5a338f0ee3e72a9dc0bed9926eacd3a0620a46f7
Hong <ihongs@live.cn>
2013-07-25 22:44:47 +0800

    重写了构建Jar的批处理

--- buildJar.bat
+++ buildJar.bat
@@ -1,10 +1,10 @@
 @echo off
-set CLSPATH=\Workspace\HongsCORE\build\web\WEB-INF\classes
-set JARNAME=hongs-core-framework
-set VERSION=0.1.2
-e:
-cd \Workspace\HongsCORE\build\web\WEB-INF\classes
-jar cvf hongs-core-0.1.2.jar app\hongs org\json\simple
-copy %JARNAME%-%VERSION%.jar \Workspace\HongsADMS\web\WEB-INF\lib\ /Y /B
-cd \Workspace\HongsCORE
+set JARNAME=hongs-core-framework-0.2.0
+set PKGPATH=app\hongs org\json\simple
+set CLSPATH=%~DP0\build\web\WEB-INF\classes
+set DSTPATH=%~DP0\dist
+cd      %CLSPATH%
+jar cvf %JARNAME%.jar %PKGPATH%
+copy    %JARNAME%.jar %DSTPATH%
+cd      %~DP0
 @echo on
