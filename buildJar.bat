598c39b3f0a1733bf5e5f2963866632e9fdf39b5
Hong <ihongs@live.cn>
2013-07-30 19:40:15 +0800

    改个名

--- buildJar.bat
+++ buildJar.bat
@@ -1,10 +1,12 @@
 @echo off
+
 set JARNAME=hongs-core-framework-0.2.0
-set PKGPATH=app\hongs org\json\simple
+set PKGLIST=app\hongs org\json\simple
 set CLSPATH=%~DP0\build\web\WEB-INF\classes
 set DSTPATH=%~DP0\dist
-cd      %CLSPATH%
-jar cvf %JARNAME%.jar %PKGPATH%
-copy    %JARNAME%.jar %DSTPATH%
-cd      %~DP0
+
+cd %CLSPATH%
+jar cvf %DSTPATH%\%JARNAME%.jar %PKGLIST%
+cd %~DP0
+
 @echo on
