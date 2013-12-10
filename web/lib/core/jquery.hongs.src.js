bee7afe05d9885bdc766d834c04514bc4a9c3fbe
Hong <kevin.hongs@gmail.com>
2013-12-10 16:02:06 +0800

    Update jquery.hongs.src.js

--- web/lib/core/jquery.hongs.src.js
+++ web/lib/core/jquery.hongs.src.js
@@ -2488,9 +2488,9 @@ $.fn.load = function(url, data, complete) {
             // 编辑标题替换
             if (cnf.title) {
                 if (H$("&id", box)) {
-                    cnf.title = hsGetLang(cnf.title, {'editTitle': cnf.update || hsGetLang("edit.title.update")});
+                    cnf.title = hsGetLang(cnf.title, {'editTitle': cnf.update || hsGetLang("form.update")});
                 } else {
-                    cnf.title = hsGetLang(cnf.title, {'editTitle': cnf.create || hsGetLang("edit.title.create")});
+                    cnf.title = hsGetLang(cnf.title, {'editTitle': cnf.create || hsGetLang("form.create")});
                 }
                 if (h.length) h.text(cnf.title);
             }
