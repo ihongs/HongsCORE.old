cce562f1a4777134179969fd394413c2ab7b494a
Hong <kevin.hongs@gmail.com>
2014-01-20 15:21:47 +0800

    add hsFillFormSelect and hsFillListSelect

--- web/app/common/hongs.select.js
+++ web/app/common/hongs.select.js
@@ -2,33 +2,52 @@
  * 扩展-选择控件
  * 作者：黄弘 <kevin.hongs@gmail.com>
  * 创建: 2013/06/20
- * 修改: 2013/06/21
- * 依赖: jquery.js, jquery.tools.js, hongs.jquery.js
+ * 修改: 2014/01/20
+ * 依赖: jquery.js, jquery.tools.js, hongs.common.js
  */
 
 (function($) {
-    self.hsInitSelect = function(tip) {
-    };
-    self.hsFillSelect = function(box, v, n, t) {
-        if (t != "info") return;
-        box.empty();
-        if (v ==  null ) return;
-        $.each(v, function(v2, t2) {
-            $('<div class="option-box"></div>')
-              .append($('<input type="hidden"')
-                .attr("name", n)
-                .val ( v2 ))
-              .append($('<span class="option-txt"></span>')
-                .text( t2 ))
-              .append($('<span class="option-btn"></span>'))
-              .appendTo(box);
+    var selectInit = function(box, dat, name) {
+        box.find(".option-box").each(function() {
+            dat["_"+$(this).find(":hidden").val()] = $(this).text();
         });
+    };
+    var selectFill = function(box, dat, name) {
+        box.empty();
+        for(var id in dat) {
+            var txt = dat[id];
+            id = id.substr(1);
+            $('<div class="option-box"></div>')
+            .append($('<input type="hidden"/>').attr("name", name).val(id))
+            .append($('<span class="option-txt"></span>').text(txt))
+            .append($('<span class="option-del"></span>'))
+            .appendTo(box);
+        }
         box.append($('<div class="cb"></div>'));
     };
+    var selectLoad = function(tip, dat, name) {
+        tip.find(".check-one").each(function() {
+            if (dat["_"+$(this).val()]) {
+                $(this).prop("checked", true );
+            }
+        });
+    };
 
+    /**
+     * 选择组件
+     * 用于在表单中选择关联、依赖的数据
+     * @param {Object} conf 配置, 可选
+     * conf 结构:
+     * {
+     *   name: 输入项名称, 默认取属性 data-select-name
+     *   url : 选择窗地址, 默认取属性 data-select-url
+     *   box : 已选项区域, 默认取属性 data-select-box, 都没有则自动找相邻的 .select-box
+     *   init: 初始函数, 解析已选数据, 在构建选择组件时执行
+     *   fill: 填充函数, 填充选中数据, 在选择窗确认选择时执行
+     *   load: 加载函数, 选中已选的列, 在选择窗加载数据后执行
+     * }
+     */
     $.fn.hsSelect = function(conf) {
-        /** 选择按钮 **/
-
         if(!$(this).hasClass("hsSelectBtn")) {
             $(this).addClass("hsSelectBtn");
         }
@@ -38,107 +57,102 @@
 
         if (!conf) conf = {};
 
-        var name = conf["name"] || $(this).attr("data-select-name");
-        var url  = conf["url"]  || $(this).attr("data-select-url" );
-        var box  = conf["box"]  || $(this).attr("data-select-box" );
-        var btn = $(this);
-        var hsFillSelect = conf["fill"] || self.hsFillSelect;
+        var init = conf["init"] || selectInit;
+        var fill = conf["fill"] || selectFill;
+        var load = conf["load"] || selectLoad;
+        var url  = conf["url"]  || $(this).attr("data-select-url");
+        var box  = conf["box"]  || $(this).attr("data-select-box");
+        var btn  = $(this);
+        var dat  = {};
 
         if (! box)
-            box = $(this).closest(".form-group" ).find(".select-box" );
+            box = btn.closest( ".form-group").find( ".select-box");
         else if (typeof box == "string")
-            box = /^\$/.test(box) ? $(box.substring(1), this) : $(box);
-
-        $(this).on("click", function() {
-            var tip = $.hsOpen(url);
-            var ids = {};
-            
-            tip.find(".cancel").click(function() {
-                tip.data("tooltip").hide();
-                return false;
-            });
-            tip.find(".ensure").click(function() {
-                btn.trigger("selectBack", [ids]);
-                tip.data("tooltip").hide();
-                return false;
-            });
-            tip.find(".check-one").change(function() {
-                var id = $(this).val();
-                if ($(this).prop("checked")) {
-                    btn.trigger("selectItem", [id, ids]);
-                    var txt = $(this).closest("tr").find(".name");
-                    ids["_"+id] = [id,txt];
-                }
-                else {
-                    if (ids["_"+id] != null) {
-                        delete ids["_"+id];
-                    }
-                }
-            });
-
-            var checked = function() {
-                var ids = tip.data("ids");
-                tip.find(".check-one").each(function() {
-                    if (ids["_"+$(this).val()]) {
-                        $(this).prop("checked", true );
-                    }
-                });
-            };
-            tip.on("loadBack", function() {
-                checked();
-            }); checked();
+            box = /^\$/.test(box) ? $(box.substr(1), btn) : $(box);
+        box.data("init", init);
+        box.data("fill", fill);
         
-            // 单选或多选
-            if ($(this).hasClass( "single")) {
-                    tip.addClass( "single")
-                          .find ("._check")
-                       .addClass( "_radio")
-                    .removeClass( "_check")
-                 .attr("data-fn", "_radio");
-            }
-            else {
-                 tip.removeClass( "single")
-                          .find ("._radio")
-                       .addClass( "_check")
-                    .removeClass( "_radio")
-                 .attr("data-fn", "_check");
-            }
+        var name = box.attr("data-fn");
 
-            var ids = {};
-            box.find(".option-box").each(function() {
-                var id = $(this).find(":hidden").val();
-                ids[ "_"+id ] = [ id, $(this).text() ];
+        init.call(btn, box, dat, name);
+
+        btn.on("click", function( ) {
+            var tip = $.hsOpen(url)
+            .toggleClass("single-select", btn.hasClass("single-select"))
+            .on("loadBack", function() {
+                load.call(btn, tip, dat, name);
+            })
+            .on("selectBack", function() {
+                fill.call(btn, box, dat, name);
+            })
+            .on("selectItem", function(id, txt) {
+                if (btn.hasClass("single-select")) {
+                    dat = {};
+                    if (txt !== undefined) {
+                        dat[id] = txt ;
+                    }
+                } else {
+                    if (txt !== undefined) {
+                        dat[id] = txt ;
+                    } else {
+                        delete dat[id];
+                    }
+                }
+            })
+            .on("click", ".cancel", function() {
+                tip.hsClose();
+                return false ;
+            })
+            .on("click", ".ensure", function() {
+                tip.trigger("selectBack");
+                tip.hsClose();
+                return false ;
+            })
+            .on("change", ".check-one", function() {
+                var id  = $(this).val();
+                var txt = $(this).prop("checked")
+                        ? $(this).closest("tr").find(".name").text()
+                        : undefined;
+                tip.trigger("selectItem", [id, txt]);
+                return false ;
             });
-
-            tip.data("btn", $(this))
-               .data("ids", ids)
-               .load( url );
-
-            $(this).tooltip({tip : tip}).show();
         });
-        $(this).on("selectBack", function() {
-            hsFillSelect(box, tip.data("ids"), name);
-            return false;
-        });
-        $(this).closest("form").on("loadBack", function(evt, rst) {
-            if (rst && rst.select && rst.select[name]) {
-                hsFillSelect(box, rst.select[name], name);
-            }
-        });
-
-        /** 选择菜单 **/
-
-        if(!tip.hasClass("hsSelectTip")) {
-            tip.addClass("hsSelectTip");
-        }
-        else {
-            return;
-        }
-
     };
     
-    // 自动初始化
+    // 自动初始化组件
     $(document).on("hsReady", ".load-box", function() {
         $(this).find(".select-btn").hsSelect();
     }); $(this).find(".select-btn").hsSelect();
+    
+    /** 扩展方法 **/
+    
+    /**
+     * 用于表单的 HsForm 组件的 _select 项填充函数
+     */
+    self.hsFillFormSelect = function(inp, v, n) {
+        inp.data("fill")(inp, v, n);
+    };
+    
+    /**
+     * 用于选择窗 HsList 组件的 _select 列填充函数
+     * 用法:
+     * 在 HsList 的 object.config 中增加参数：
+     * <param name="_fill__select" value="(_fill__select)" />
+     * 然后在列表 head 的选择列加上属性:
+     * data-tn="_select"
+     * @param {Element} td 当前要填充的格子
+     * @param {String} v 在此无意义
+     * @param {String} n 在此无意义
+     * @context HsList 实例
+     */
+    self.hsFillListSelect = function(td, v, n) {
+        if (this._single_select === undefined) {
+            this._single_select = td.closest(".load-box").hasClass("single-select");
+        }
+        if (this._single_select) {
+            HsList.prototype._fill__radio.call(this, td, v, n);
+        } else {
+            HsList.prototype._fill__check.call(this, td, v, n);
+        }
+    };
 })( jQuery );
