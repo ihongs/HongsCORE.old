f1529c8000165a19f7e5fcd57ee0b70e67199111
Hongs <ihongs@live.cn>
2015-05-30 09:48:57 +0800

    update

--- hongs-web/web/WEB-INF/bin/run.bat
+++ hongs-web/web/WEB-INF/bin/run.bat
@@ -4,11 +4,11 @@ set CORE_PATH=%~DP0\..
 set JAVA_PATH="%JAVA_HOME%\bin\java"
 set KLASSPATH=%CLASSPATH%;%CORE_PATH%\lib\*;%CORE_PATH%\lib\classes;%CORE_PATH%\classes
 
-"%JAVA_PATH%" %JAVA_OPTS%  ^
-  -classpath "%KLASSPATH%" ^
-  -Dlog4j.configurationFile="%CORE_PATH%\lib\classes\log4j2.xml" ^
-  -Ddirs.log="%CORE_PATH%\var\log" ^
-  app.hongs.cmdlet.CmdletRunner %* ^
+"%JAVA_PATH%" %JAVA_OPTS% ^
+  -classpath "%KLASSPATH%"^
+  -Dlog4j.configurationFile="%CORE_PATH%\lib\classes\log4j2.xml"^
+  -Dlogs.dir="%CORE_PATH%\var\log"^
+  app.hongs.cmdlet.CmdletRunner %*^
   --corepath "%CORE_PATH%"
 
 @echo on
